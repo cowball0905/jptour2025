fc85312a596914e7057e431eb4ce48c39a5a58b7
sky <cyng268-c@my.cityu.edu.hk>
2025-06-29 15:56:04 +0800

    reduce portrait image size

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def process_images_recursively(root_path):
 # Usage
 if __name__ == "__main__":
     # Replace with your actual path
-    image_directory = "./assets/img/contents"
+    image_directory = "./assets/img/Prepare"
     
     # Confirm before processing
     print(f"This will process all WebP images in: {image_directory}")
